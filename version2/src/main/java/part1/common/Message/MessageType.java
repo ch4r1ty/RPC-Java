2b32c887da71b84f106c700fb23967d3f362a711
ch4r1ty <920853279@qq.com>
2025-02-25 08:36:51 -0500

    version2-part1

--- version2/src/main/java/part1/common/Message/MessageType.java
+++ version2/src/main/java/part1/common/Message/MessageType.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
  * @version 1.0
  * @create 2024/6/2 22:29
  */
+
+//enum（枚举）是一种特殊的类，用于定义 一组固定的常量。
 @AllArgsConstructor
 public enum MessageType {
     REQUEST(0),RESPONSE(1);
